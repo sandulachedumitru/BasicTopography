aa664dd38756677e41e9ac84182c7cf792c8d1ad
dumitru.sandulache <dumitru.sandulache@A6-pc.ama.org>
2017-08-02 18:12:22 +0300

    Commit 12 - Create metric classes

---
+++ src/main/java/com/miticorp/topography/basic/model/DistanceTypeMetricKilometer.java
@@ -0,0 +1,5 @@
+package com.miticorp.topography.basic.model;
+
+public class DistanceTypeMetricKilometer extends DistanceTypeMetric {
+	// TODO
+}
---
+++ src/main/java/com/miticorp/topography/basic/model/DistanceTypeMetricMillimeter.java
@@ -0,0 +1,5 @@
+package com.miticorp.topography.basic.model;
+
+public class DistanceTypeMetricMillimeter extends DistanceTypeMetric {
+	// TODO
+}
